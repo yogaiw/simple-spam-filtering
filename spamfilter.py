4dd872fde1655aff4de83172470603e8a16694c3
yogaiw <agoy.indra@gmail.com>
2021-04-17 17:30:53 +0700

    hasil sementara

--- spamfilter.py
+++ spamfilter.py
@@ -6,21 +6,25 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 
+# memanggil dataset
 dataset = open("spam.txt","r")
 spamlib = dataset.read().split(",")
 ## print(spamlib)
 dataset.close()
 
-msg = "selamat anda mendapatkan pinjaman dengan jumlah yang wow dan wow"
+# input teks untuk deteksi spam
+msg = "Selamat anda mendapatkan kesempatan pinjaman dengan jumlah yang wow, jangan sia siakan kesempatan ini, anda dapat melakukan kredit motor secara cepat"
 tokens = word_tokenize(msg.lower())
 print(tokens)
 
+# fungsi untuk vektor
 def vectorize(tokens):
     vector=[]
     for w in spamlib:
         vector.append(tokens.count(w))
     return vector
 
+# membersihkan stop words dari input
 stopwords = stopwords.words('indonesian')
 msg_sw = []
 for word in tokens:
@@ -28,9 +32,17 @@ for word in tokens:
         msg_sw.append(word)
 print(msg_sw)
 
+# menghitung kata yang mengandung spam, dan vektorisasi
 spam_indicated = []
 for word in msg_sw:
     if word in spamlib:
         spam_indicated.append(word)
 print(spam_indicated)
-print(vectorize(msg_sw))
+vector = vectorize(msg_sw)
+print(vector)
+
+def result(v):
+    spam_contain_count = sum(v)*20
+    return spam_contain_count
+
+print(str(result(vector))+"%")
